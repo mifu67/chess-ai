09deed4203c04daae4908aa6342e4535fcb2b6f7
josephhzhangg <56054525+josephhzhangg@users.noreply.github.com>
2022-05-18 11:07:39 -0700

    other updates to minimax

--- eval.py
+++ eval.py
@@ -17,8 +17,11 @@ class Eval():
         self.pieces_weights = [1, 3, 3, 5, 9, 1]
 
     # Param:: player_color - chess.COLOR of player, either chess.WHITE or chess.BLACK
-    def simple_eval(self, player_color):
+    #def simple_eval(self, player_color):
+    def evaluation_function(self, board, player_color):
+        pieces_weights = [1, 3, 3, 5, 9, 1]
         count = 0
         for i in range(len(PIECES)):
-            count += self.pieces_weights[i] * len(self.board.pieces(PIECES[i], player_color))
+            count += pieces_weights[i] * len(self.board.pieces(PIECES[i], player_color))
         return count
+
