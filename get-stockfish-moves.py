f88627272eb2376ee90dc3e7c6376de9b5fd8e55
mifu67 <mechllfu@gmail.com>
2022-06-01 12:35:48 -0700

    created stockfish-moves.txt

---
+++ get-stockfish-moves.py
@@ -0,0 +1,35 @@
+from stockfish import Stockfish
+import os
+
+NUM_MOVES = 30 
+
+def main():
+    stockfish = Stockfish()
+    directory = "fenData"
+    
+    with open('stockfish-moves.txt', 'a') as f:
+        f.write("test")
+    f.close()
+
+    for file_name in os.listdir(directory):
+        file_path = os.path.join(directory, file_name)
+        if os.path.isfile(file_path):
+            with open(file_path) as pgn_file:
+                lines = pgn_file.readlines()
+
+                for fen in lines:
+                    stockfish.set_fen_position(fen.strip())
+                    best_n_moves = stockfish.get_top_moves(NUM_MOVES)
+                    moves_string = ""
+
+                    for move in best_n_moves:
+                        moves_string += move['Move'] + " "
+                    print(moves_string)
+                    moves_string += '\n'
+                    
+                    with open('stockfish-moves.txt', 'a') as f:
+                        f.write(moves_string)
+
+                        
+if __name__ == "__main__":
+    main()
