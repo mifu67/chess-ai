113d659f598a777053121a147e37901012fd905b
mifu67 <mechllfu@gmail.com>
2022-06-03 01:05:45 -0700

    fixed bug where you couldn't couldn't checkmate

--- minimax.py
+++ minimax.py
@@ -123,12 +123,14 @@ class MinimaxAgent:
                     return minValue
 
         legalMoves = self.order_moves(list(self.board.legal_moves))
+        print(legalMoves)
         # legalMoves = self.board.legal_moves
-        maxAction = ""
+        maxAction = legalMoves[0]
         maxValue = -math.inf
         alpha = -math.inf
         beta = math.inf
         for action in legalMoves:
+            print(action)
             self.board.push(action)
             value = alphaBeta(self.board, not self.isComputer, self.depth, alpha, beta)
             self.board.pop()
@@ -137,5 +139,7 @@ class MinimaxAgent:
                 maxAction = action
                 maxValue = value
                 alpha = max(alpha, maxValue)
+        print("max action:", maxAction)
+        print("SAN:", self.board.san(maxAction))
 
         return maxAction
